611ec5c35fec558a2c7e934a5ec4499a9a3d1b14
Castle <felipebenavides03@gmail.com>
2020-08-25 11:18:01 -0500

    explicacion y ejercicios de veriables

---
+++ variables.js
@@ -0,0 +1,82 @@
+console.log("hola mundo");
+var nombre = "felipe" , apellido = "CASTILLO" ;
+
+console.log("hola " + nombre +" " + apellido);
+
+// variables string
+
+var nombreenmayusculas= nombre.toUpperCase()
+var apellidoenminusculas = apellido.toLowerCase()
+
+console.log(" nombre:"+ nombreenmayusculas +"_____ apellido: " + apellidoenminusculas)
+
+var primeraletradelvariable = nombre.charAt(0)
+
+console.log(" primera letra de la variable nombre : " + primeraletradelvariable)
+
+var cantidaddeletrasdeunavariable = nombre.length;
+
+console.log("la cantidad de letras de la vriable nombre es :" + cantidaddeletrasdeunavariable)
+
+var concatenarvariable =`${nombre} ${apellido}`
+
+console.log("variables concatenadas con comillas invertidas:"+ concatenarvariable)
+
+var sudstring = nombre.substr(1 ,3)
+
+console.log( "la seleccion de sub strings de una variable  " + sudstring)
+
+
+// variable numeros
+
+var edad =20;
+
+//edad = edad + 1 ;
+edad += 1
+console.log(edad)
+
+var peso = 73
+ // peso = peso - 3
+
+ peso-= 3
+console.log(peso)
+
+var comida = 1;
+
+peso = peso + comida
+console.log(peso)
+
+var ejercicio = 3
+
+peso -=  ejercicio;
+
+console.log(peso)
+
+
+var vino = 200.3
+
+var totalmal= vino * 3;
+
+console.log(totalmal);
+
+ var total = Math.round(vino * 100 * 3 ) / 100
+
+ console.log(total);
+
+ vinostr = total.toFixed(4)
+
+ console.log("agrega 4 caracteres despues del numero entero "+vinostr);
+
+var totalnum = parseFloat(vinostr)
+
+console.log(totalnum)
+
+// funciones 
+
+var nombre = luis, edad = 20;
+
+function imprimiredad(){
+    console.log(`${nombre} tiene ${edad} años`)
+}
+
+imprimiredad();
